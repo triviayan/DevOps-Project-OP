a8d9ebdb42f0e2bfd0ed29297b8c272543ae603a
Omkar Policepatel <omkar.patel@cloud4c.com>
2024-07-24 12:19:02 +0530

    added

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{message}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
